e843e22e15f3395e780b74e6899a32cfb334a7f6
ilkeruzer <ilkeruzer35@gmail.com>
2021-04-23 19:35:53 +0300

    Get all coin is call

--- app/src/main/java/com/ilkeruzer/cointicker/data/remote/IService.kt
+++ app/src/main/java/com/ilkeruzer/cointicker/data/remote/IService.kt
@@ -1,7 +1,10 @@
 package com.ilkeruzer.cointicker.data.remote
 
+import com.ilkeruzer.cointicker.data.remote.model.CoinResponse
 import retrofit2.http.GET
 
 interface IService {
 
+    @GET("coins/list")
+    suspend fun coinList(): List<CoinResponse>
 }
--- app/src/main/java/com/ilkeruzer/cointicker/di/Module.kt
+++ app/src/main/java/com/ilkeruzer/cointicker/di/Module.kt
@@ -1,11 +1,17 @@
 package com.ilkeruzer.cointicker.di
 
+import com.ilkeruzer.cointicker.data.remote.NetworkModule
+import com.ilkeruzer.cointicker.ui.MainViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
+
+    single { NetworkModule() }
 
 }
 
 val viewModelModule = module {
 
+    viewModel { MainViewModel(get()) }
 }
